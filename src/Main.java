9b6a317bb71f0ab51ac6769b81b4a94f592f82f2
naya <25nsangoram@castilleja.org>
2023-05-16 11:14:28 -0700

    I created a Question class to store 4 Option objects and a String that holds the question. Each option has a season attributed to it. For example, a favorite sport of skiing relates to the season Winter. I'm still deciding where I'm going to store the Question objects and how to display them.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import processing.core.PApplet;
+import java.util.ArrayList;
+public class Main extends PApplet {
+    private ArrayList <Question> listQ;
+    public static void main(String[] args) {
+
+    }
+    public Main() {//Main's constructor
+        super();
+        //app = this;
+    }
+    public void settings() {
+        size(600, 600);
+
+    }
+}
---
+++ src/Question.java
@@ -0,0 +1,13 @@
+public class Question {
+    private String questions;
+    private Option a;
+    private Option b;
+    private Option c;
+    private Option d;
+    public Question(){
+
+    }
+    public Question (int optionA, int optionB  ){
+
+    }
+}
