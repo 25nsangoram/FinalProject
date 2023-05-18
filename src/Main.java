9637541935b90c6052c7134e010630dc38455700
naya <25nsangoram@castilleja.org>
2023-05-18 11:26:02 -0700

    Today in class, I worked in Main and more specifically in setup() (lines 24-37). Using Ms. Greyson's advice, I created a 2D array of Strings. Each row contains String information that will correspond to a specific question. The first column of each row contains the question name in the form of a String.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import processing.core.PApplet;
 import java.util.ArrayList;
 public class Main extends PApplet {
     private ArrayList <Question> listQ;
+    private String [][] dataSet;
+    private Question thisQuestion;
     public static Main app;
     public static void main(String[] args) {
         PApplet.main("Main");
@@ -19,14 +21,30 @@ public class Main extends PApplet {
 
     }
     public void setup(){
-
-    }
+        dataSet = new String [][] {{"Question1", "answer1", "answer2", "answer3", "answer4"}, {"Question2", "answer1", "answer2", "answer3", "answer4"}};
+        for(int row = 0; row < dataSet.length; row++){
+            for(int col = 0; row < dataSet[row].length; row++){
+                Option a = new Option (1, dataSet[row][1]);
+                Option b = new Option (2, dataSet[row][2]);
+                Option c = new Option (3, dataSet[row][3]);
+                Option d = new Option (4, dataSet[row][4]);
+                ArrayList <Option> optionSet = new ArrayList <Option>();
+                optionSet.add(a);
+                optionSet.add(b);
+                optionSet.add(c);
+                optionSet.add(d);
+                //String [] optionSet = new String [] {dataSet[row][1], dataSet[row][2], dataSet[row][3], dataSet[row][4]};
+                thisQuestion = new Question(dataSet[row][0],optionSet);
+            }
+            }
+        }
     public void draw() {
         background(1,1,1);
         text("Let me guess your favorite season!", width/2,height/2);
+        //have a background call to erase
+
 
     }
     public void mouseClicked(){
-
     }
 }
--- src/Question.java
+++ src/Question.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 public class Question {
     private String questions;
     private ArrayList <Option> options;
+
     public Question(){
 
     }
@@ -10,4 +11,26 @@ public class Question {
         this.options= options;
 
     }
+    public void display(){
+        Main.app.fill(23,23,23);
+        Main.app.ellipse(600/4,600/4,50,50);
+
+        Main.app.ellipse(2*(600/4),2*(600/4),50,50);
+        Main.app.ellipse(3*(600/4),3*(600/4),50,50);
+        Main.app.ellipse(600/4,600/4,50,50);
+
+    }
+    public Option getA(){
+        return options.get(0);
+    }
+    public Option getB(){
+        return options.get(1);
+    }
+    public Option getC(){
+        return options.get(2);
+    }
+    public Option getD(){
+        return options.get(3);
+    }
+
 }
